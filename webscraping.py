2e7697a334a2d963990d9c24986f9c77461aba10
Anil Yasar <anlysr2002@gmail.com>
2025-01-31 13:09:13 +0300

    navigate through all pages with the help of page count info, get all book urls

--- webscraping.py
+++ webscraping.py
@@ -34,9 +34,28 @@ def find_page_count(driver, url):
         print("Hata:", e)
         return None
 
+def get_book_urls(driver, url, max_pagination):
+    book_elements_xpath = "//div[@class = 'image_container']//a"
+    book_urls = []
+
+    for i in range(1, max_pagination + 1):
+        update_url = url if url == 1 else url.replace("index", f"catalogue/page-{i}")
+        driver.get(update_url)
+        time.sleep(SLEEP_TIME)
+        book_elements = driver.find_elements(By.XPATH, book_elements_xpath)
+        if not book_elements:
+            break
+
+        temp_urls = [element.get_attribute("href") for element in book_elements]
+        book_urls.extend(temp_urls)
+
+    return book_urls
+
 def main():
     driver = initialize_driver()
     page_count = find_page_count(driver, BASE_URL)
+    book_urls = get_book_urls(driver, BASE_URL, page_count)
+    print(len(book_urls))
 
 if __name__ == "__main__":
     main()
